bcfb932286e6c145f1dae97a6475156f19f599ae
Abhishek Bhatt <abhibhatt587@gmail.com>
2025-12-20 10:58:59 +0530

    Make Home, navbar and resume card section

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,35 @@
+import ResumeCard from "~/components/ResumeCard";
+import { resumes } from "../../constants/";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/Navbar";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "AI Resume Analyzer" },
+    { name: "description", content: "Smart Feedbake for you dream job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+      <section className="main-seciton">
+        <div className="page-heading py-16">
+          <h1 className="sm:max-w-3xl">
+            Track Your Application & Resume Ratings
+          </h1>
+          <h2>Review your submissions and check AI-powered feedback.</h2>
+        </div>
+
+        {resumes.length > 0 && (
+          <div className="resumes-section">
+            {resumes.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume} />
+            ))}
+          </div>
+        )}
+      </section>
+    </main>
+  );
 }

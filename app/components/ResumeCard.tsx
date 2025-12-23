1f1dd5faafbe7ba41c5db3f17c20ad0aab1fde67
Abhishek Bhatt <abhibhatt587@gmail.com>
2025-12-23 17:58:41 +0530

    Implement the wipe functionality.

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -1,16 +1,16 @@
-import {Link} from "react-router";
+import { Link } from "react-router";
 import ScoreCircle from "~/components/ScoreCircle";
-import {useEffect, useState} from "react";
-import {usePuterStore} from "~/lib/puter";
+import { useEffect, useState } from "react";
+import { usePuterStore } from "~/lib/puter";
+
 
 const ResumeCard = ({ resume: { id, companyName, jobTitle, feedback, imagePath } }: { resume: Resume }) => {
-    const { fs } = usePuterStore();
     const [resumeUrl, setResumeUrl] = useState('');
-
+    const { fs } = usePuterStore();
     useEffect(() => {
         const loadResume = async () => {
             const blob = await fs.read(imagePath);
-            if(!blob) return;
+            if (!blob) return;
             let url = URL.createObjectURL(blob);
             setResumeUrl(url);
         }
@@ -22,11 +22,11 @@ const ResumeCard = ({ resume: { id, companyName, jobTitle, feedback, imagePath }
         <Link to={`/resume/${id}`} className="resume-card animate-in fade-in duration-1000">
             <div className="resume-card-header">
                 <div className="flex flex-col gap-2">
-                    {companyName && <h2 className="!text-black font-bold break-words">{companyName}</h2>}
-                    {jobTitle && <h3 className="text-lg break-words text-gray-500">{jobTitle}</h3>}
-                    {!companyName && !jobTitle && <h2 className="!text-black font-bold">Resume</h2>}
+                    {companyName && <h2 className="text-black! font-bold wrap-break-word">{companyName}</h2>}
+                    {jobTitle && <h3 className="text-lg wrap-break-word text-gray-500">{jobTitle}</h3>}
+                    {!companyName && !jobTitle && <h2 className="text-black! font-bold">Resume</h2>}
                 </div>
-                <div className="flex-shrink-0">
+                <div className="shrink-0">
                     <ScoreCircle score={feedback.overallScore} />
                 </div>
             </div>
@@ -40,7 +40,7 @@ const ResumeCard = ({ resume: { id, companyName, jobTitle, feedback, imagePath }
                         />
                     </div>
                 </div>
-                )}
+            )}
         </Link>
     )
 }
